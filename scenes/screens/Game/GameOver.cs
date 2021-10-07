0c2cce8a7d180a91878b17ee3002c4387408f04e
Denis BOURGE <denis.bourge@sharingcloud.com>
2021-10-07 22:38:25 +0200

    feat: use betterblur

--- scenes/screens/Game/GameOver.cs
+++ scenes/screens/Game/GameOver.cs
@@ -5,20 +5,20 @@ public class GameOver : CanvasLayer
 {
     private Button _RestartButton;
     private AnimationPlayer _AnimationPlayer;
-    private GaussianBlur _GaussianBlur;
+    private BetterBlur _BetterBlur;
 
     public override void _Ready()
     {
         _AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         _RestartButton = GetNode<Button>("Play");
-        _GaussianBlur = GetNode<GaussianBlur>("GaussianBlur");
+        _BetterBlur = GetNode<BetterBlur>("BetterBlur");
 
         _RestartButton.Connect("pressed", this, nameof(RestartGame));
     }
 
     public void Start()
     {
-        _GaussianBlur.Visible = true;
+        _BetterBlur.Visible = true;
         _AnimationPlayer.Play("idle");
         _RestartButton.GrabFocus();
     }
